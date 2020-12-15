4d56217fdfe720ed922bd785cb7b90d8314b127c
Deepak pundir <sendit2deepak@gmail.com>
2020-12-15 19:39:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-function App() {
-  return (
+import React from "react";
+//import React from "react";
+import { render } from "react-dom";
+//import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import About from "./Components/About";
+import Home from "./Components/Home";
+import Topics from "./Components/Topics";
+
+const App = () => (
+  <Router>
     <div>
-      <h1>Hi I am blank App</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
