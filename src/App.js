fcad23c7c7e83c467dc004a2d940390419d86221
Deepak pundir <sendit2deepak@gmail.com>
2021-01-18 19:35:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React from "react";
-//import React from "react";
-import { render } from "react-dom";
-//import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import About from "./Components/About";
-import Home from "./Components/Home";
-import Topics from "./Components/Topics";
-
-const App = () => (
-  <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
-      <hr />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
-    </div>
-  </Router>
-);
+class App extends Component {
+  componentDidMount() {
+    setInterval(() => {
+      let randomBgColor = Math.floor(Math.random() * 16772215).toString(16);
+      document.body.style.backgroundColor = `#${randomBgColor}`;
+      console.log(randomBgColor);
+    }, 500);
+  }
+  render() {
+    return <div className="App">Hello</div>;
+  }
+}
 
 export default App;
