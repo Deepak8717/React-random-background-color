431ebeec985c2bc2d1cad6965f3d1c1d371a1b40
Deepak pundir <sendit2deepak@gmail.com>
2021-01-18 19:42:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-class App extends Component {
+class App extends React.Component {
   componentDidMount() {
     setInterval(() => {
-      let randomBgColor = Math.floor(Math.random() * 16772215).toString(16);
+      let randomBgColor = Math.floor(Math.random() * 15673879).toString(16);
       document.body.style.backgroundColor = `#${randomBgColor}`;
+      document.body.style.transition = "1s ease";
       console.log(randomBgColor);
-    }, 500);
+    }, 10000);
   }
   render() {
     return <div className="App">Hello</div>;
